3c6c012396673b26f8799895f4a57c3e5461bfc8
Jon Brenner <jonny.m.brenner@gmail.com>
2017-04-09 16:11:56 -0500

    Adds lookup() method and changes all value to key

--- bst.py
+++ bst.py
@@ -1,35 +1,41 @@
 class EmptyTree:
     def empty(self):
-        return True                                                              
-                                                                                 
+        return True
+
     def __len__(self):
-        return 0                                                                 
-                                                                                  
-class BST:                                                                       
-    def __init__(self, value):                                
-        self._value = value                                                       
-        self._left = EmptyTree()                                                 
-        self._right = EmptyTree()                                                 
-                                                                                  
-    def empty(self):                                                             
-        return False                                                             
-                                                                                  
-    def insert(self, value):                                                     
-        if value > self._value:
+        return 0
+
+    def lookup(self, key):
+        return False
+
+class BST:
+    def __init__(self, key):
+        self._key = key
+        self._left = EmptyTree()
+        self._right = EmptyTree()
+
+    def empty(self):
+        return False
+
+    def insert(self, key):
+        if key > self._key:
             if self._right.empty():
-                self._right = BST(value)
+                self._right = BST(key)
             else:
-                self._right.insert(value)
-        elif value < self._value:
+                self._right.insert(key)
+        elif key < self._key:
             if self._left.empty():
-                self._left = BST(value)
+                self._left = BST(key)
             else:
-                self._left.insert(value)                                              
-            return                                                                
-        else:                                 
-            raise ValueError(str(value))
+                self._left.insert(key)
+            return
+        else:
+            raise keyError(str(key))
 
     def __len__(self):
         return 1 + len(self._right) + len(self._left)
 
+    def lookup(self, key):
+        return (self._key == key or self._right.lookup(key)
+                or self._left.lookup(key))
 
