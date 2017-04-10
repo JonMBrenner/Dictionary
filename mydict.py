15d8715ad8ed69a24ce20aa1fd0b7b764de1d05e
Jon Brenner <jonny.m.brenner@gmail.com>
2017-04-09 19:08:30 -0500

    Comments out everything except getitem and setitem

--- mydict.py
+++ mydict.py
@@ -1,3 +1,5 @@
+import bst
+
 class Dict:
     def __init__(self):
         self._elements = []
@@ -16,24 +18,24 @@ class Dict:
                 return v
         raise KeyError(str(key))
 
-    def __contains__(self, key):
-        return any(k == key for k, v in self._elements)
-
-    def __len__(self):
-        return len(self._elements)
-
-    def __repr__(self):
-        return ('{'  + ', '.join('{}: {}'.format(
-            repr(k), repr(v)) for k, v in self._elements) + '}')
-
-    def keys(self):
-        return (k for k, _ in self._elements)
-
-    def values(self):
-        return (v for _, v in self._elements)
-
-    def items(self):
-        return zip(self.keys(), self.values())
+#    def __contains__(self, key):
+#        return any(k == key for k, v in self._elements)
+#
+#    def __len__(self):
+#        return len(self._elements)
+#
+#    def __repr__(self):
+#        return ('{'  + ', '.join('{}: {}'.format(
+#            repr(k), repr(v)) for k, v in self._elements) + '}')
+#
+#    def keys(self):
+#        return (k for k, _ in self._elements)
+#
+#    def values(self):
+#        return (v for _, v in self._elements)
+#
+#    def items(self):
+#        return zip(self.keys(), self.values())
 
 import unittest
 
@@ -52,65 +54,65 @@ class TestDict(unittest.TestCase):
       d['a'] = 3
       self.assertEqual(d['a'], 3)
 
-    def test_contains_key(self):
-      d = Dict()
-      d['abc'] = 4
-      d['ryansux'] = 42069
-      self.assertTrue('ryansux' in d)
-      self.assertFalse('theseunitestsaresoimmature' in d)
-      self.assertTrue('abc' in d)
-      self.assertFalse(4 in d)
-      self.assertFalse('a' in d)
-
-    def test_keyerror_on_not_found(self):
-      d = Dict()
-      with self.assertRaises(KeyError):
-        d['a']
-
-    def test_length(self):
-        d = Dict()
-        self.assertEqual(len(d), 0)
-        d['a'] = 1
-        d['b'] = 2
-        self.assertEqual(len(d), 2)
-        d['b'] = 3
-        self.assertEqual(len(d), 2)
-
-    def test_str(self):
-      d = Dict()
-      d['a'] = 2
-      self.assertEqual(str(d), "{'a': 2}")
-      d['abc'] = 3
-      # order doesn't matter
-      self.assertIn(str(d), ("{'a': 2, 'abc': 3}", "{'abc': 3, 'a', 2}"))
-
-    def test_keys(self):
-        d = Dict()
-        d['abc'] = 50
-        d['xyz'] = 100
-        d['hello'] = 150
-        self.assertNotIsInstance(d.keys(), list)
-        keys = sorted(list(d.keys()))
-        self.assertEqual(keys, ['abc', 'hello', 'xyz'])
-
-    def test_values(self):
-        d = Dict()
-        d['abc'] = 50
-        d['xyz'] = 100
-        d['hello'] = 150
-        self.assertNotIsInstance(d.values(), list)
-        vals = sorted(list(d.values()))
-        self.assertEqual(vals, [50, 100, 150])
-
-    def test_items(self):
-        d = Dict()
-        d['abc'] = 50
-        d['xyz'] = 100
-        d['hello'] = 150
-        self.assertNotIsInstance(d.items(), list)
-        items = sorted(list(d.items()))
-        self.assertIsInstance(items[0], tuple)
-        self.assertEqual(items, [('abc', 50), ('hello', 150), ('xyz', 100)])
+#    def test_contains_key(self):
+#      d = Dict()
+#      d['abc'] = 4
+#      d['ryansux'] = 42069
+#      self.assertTrue('ryansux' in d)
+#      self.assertFalse('theseunitestsaresoimmature' in d)
+#      self.assertTrue('abc' in d)
+#      self.assertFalse(4 in d)
+#      self.assertFalse('a' in d)
+#
+#    def test_keyerror_on_not_found(self):
+#      d = Dict()
+#      with self.assertRaises(KeyError):
+#        d['a']
+#
+#    def test_length(self):
+#        d = Dict()
+#        self.assertEqual(len(d), 0)
+#        d['a'] = 1
+#        d['b'] = 2
+#        self.assertEqual(len(d), 2)
+#        d['b'] = 3
+#        self.assertEqual(len(d), 2)
+#
+#    def test_str(self):
+#      d = Dict()
+#      d['a'] = 2
+#      self.assertEqual(str(d), "{'a': 2}")
+#      d['abc'] = 3
+#      # order doesn't matter
+#      self.assertIn(str(d), ("{'a': 2, 'abc': 3}", "{'abc': 3, 'a', 2}"))
+#
+#    def test_keys(self):
+#        d = Dict()
+#        d['abc'] = 50
+#        d['xyz'] = 100
+#        d['hello'] = 150
+#        self.assertNotIsInstance(d.keys(), list)
+#        keys = sorted(list(d.keys()))
+#        self.assertEqual(keys, ['abc', 'hello', 'xyz'])
+#
+#    def test_values(self):
+#        d = Dict()
+#        d['abc'] = 50
+#        d['xyz'] = 100
+#        d['hello'] = 150
+#        self.assertNotIsInstance(d.values(), list)
+#        vals = sorted(list(d.values()))
+#        self.assertEqual(vals, [50, 100, 150])
+#
+#    def test_items(self):
+#        d = Dict()
+#        d['abc'] = 50
+#        d['xyz'] = 100
+#        d['hello'] = 150
+#        self.assertNotIsInstance(d.items(), list)
+#        items = sorted(list(d.items()))
+#        self.assertIsInstance(items[0], tuple)
+#        self.assertEqual(items, [('abc', 50), ('hello', 150), ('xyz', 100)])
 
 if __name__ == '__main__':
     unittest.main()
